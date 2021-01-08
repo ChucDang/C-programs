f62f5a89f1399edfdfa44d4a0cfee419d9cc8569
Đặng Văn Chức <violympic1998@gmail.com>
2021-01-08 09:51:30 +0700

    Xoa bien i va xoa printf

--- 1.c
+++ 1.c
@@ -1,10 +1,9 @@
 #include "1VecToBitAsscii.c"
-int main(){
+int main()
+{
 	Set A;
 	Make_Null_Set(&A);
 	printf("\nNhap vao tap hop A:");
 	Read_Set(&A);
 	Print_Set(A);
-	int i;
-	printf("%d",i);
 }
